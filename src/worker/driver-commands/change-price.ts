b86bec7e5e5125e149ca9e026795b9ff3e3df839
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-05-03 13:32:40 +0200

    refactor: use a different name for driver command's type

--- src/worker/driver-commands/change-price.ts
+++ src/worker/driver-commands/change-price.ts
@@ -1,6 +1,6 @@
 import { createAppRequestResponsePair } from "@app/messaging";
 
 export const changePriceDriverCommand = createAppRequestResponsePair<
-  "CHANGE_PRICE",
+  "DRIVER/CHANGE_PRICE",
   { newPrice: string; offerEditUrl: string }
->("CHANGE_PRICE");
+>("DRIVER/CHANGE_PRICE");
