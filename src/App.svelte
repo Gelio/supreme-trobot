70d34db429e105f3d00546133a90e2816ed3125f
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-05-03 18:35:06 +0200

    feat: add a link to open the extension page

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,8 @@
   import type { WorkerState } from "./worker";
 
   let workerState: WorkerState | null = null;
+  const isExtensionPage = window.location.search.includes("fullpage");
+  const extensionPageUrl = chrome.runtime.getURL("index.html?fullpage");
 
   onMount(() => {
     connect((state) => {
@@ -14,6 +16,13 @@
 
 <div class="App">
   <button on:click={reloadExtension}>Reload extension</button>
+  {#if !isExtensionPage}
+    <div>
+      <a href={extensionPageUrl} target="_blank" rel="noreferrer noopener"
+        >Open extension page</a
+      >
+    </div>
+  {/if}
   <p>Worker status: {workerState?.status.type}</p>
   <button on:click={scanAllegro}>Scan Allegro</button>
 
@@ -25,7 +34,9 @@
           <!-- Unsafe mutation of the offers array. TODO: use local state for the input -->
 
           <input bind:value={offer.price} />
-          <button on:click={() => changePrice(offer, offer.price)}>Change price</button>
+          <button on:click={() => changePrice(offer, offer.price)}
+            >Change price</button
+          >
         </li>
       {/each}
     </ul>
