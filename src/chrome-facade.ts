ded8477706e4c8dc3e549494b17905b6c475e37b
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-02-26 22:41:01 +0100

    feat: allow cancellation for waitForTabToBeReady
    Add cancellation capabilities to the `waitForTabToBeReady` utility
    function.

--- src/chrome-facade.ts
+++ src/chrome-facade.ts
@@ -1,15 +1,30 @@
 // Functions that ease the use of the chrome API
 
+import { option, task, taskOption } from "fp-ts";
+import { pipe } from "fp-ts/function";
+import { Observable } from "rxjs";
+
 // TODO: use a _promisify_ helper to avoid writing types by hand
 
+/**
+ * @deprecated Use createTabTask instead
+ */
 export function createTab(
   properties: chrome.tabs.CreateProperties
 ): Promise<chrome.tabs.Tab> {
-  return new Promise((resolve) => {
-    chrome.tabs.create(properties, resolve);
-  });
+  return createTabTask(properties)();
 }
 
+export const createTabTask =
+  (properties: chrome.tabs.CreateProperties): task.Task<chrome.tabs.Tab> =>
+  () =>
+    new Promise((resolve) => {
+      chrome.tabs.create(properties, resolve);
+    });
+
+/**
+ * @deprecated Use `updateTabTask` instead
+ */
 export function updateTab(
   tabId: number,
   properties: chrome.tabs.UpdateProperties
@@ -21,11 +36,47 @@ export function updateTab(
   });
 }
 
+export const updateTabTask =
+  (
+    tabId: number,
+    properties: chrome.tabs.UpdateProperties
+  ): taskOption.TaskOption<chrome.tabs.Tab> =>
+  () =>
+    new Promise((resolve) => {
+      chrome.tabs.update(tabId, properties, (tab) =>
+        resolve(option.fromNullable(tab))
+      );
+    });
+
 export function closeTab(tabId: number): Promise<void> {
   return new Promise((resolve) => {
     chrome.tabs.remove(tabId, resolve);
   });
 }
+
+export const closeTabTask =
+  (tabId: number): task.Task<void> =>
+  () =>
+    new Promise((resolve) => {
+      chrome.tabs.remove(tabId, resolve);
+    });
+
+export const withNewTab = <T>(
+  properties: chrome.tabs.CreateProperties,
+  callback: (tabId: number) => task.Task<T>
+) =>
+  pipe(
+    createTabTask(properties),
+    // SAFETY: the created tab must have an `id` field defined
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    task.map((tab) => tab.id!),
+    task.chain((tabId) =>
+      pipe(
+        callback(tabId),
+        task.chainFirst(() => closeTabTask(tabId))
+      )
+    )
+  );
 
 /**
  * Listens to an event listener and invokes `callback` on each event.
@@ -51,3 +102,16 @@ export function listen<F extends (...args: any[]) => any, R>(
     event.addListener(listener as F);
   });
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getEvent$ = <F extends (...args: any[]) => any>(
+  event: chrome.events.Event<F>
+) =>
+  new Observable<Parameters<F>>((subscriber) => {
+    const listener = (...args: unknown[]) => {
+      subscriber.next(args as Parameters<F>);
+    };
+    event.addListener(listener as F);
+
+    return () => event.removeListener(listener as F);
+  });
--- src/marketplaces/common/messaging/tab-ready.ts
+++ src/marketplaces/common/messaging/tab-ready.ts
@@ -1,20 +1,45 @@
-import { listen } from "@app/chrome-facade";
+import { getEvent$ } from "@app/chrome-facade";
 import { extensionId } from "@app/consts";
 import { AppMessage, createMessageDescription } from "@app/messaging";
+import { task, taskEither } from "fp-ts";
+import { pipe } from "fp-ts/function";
+import { filter, map } from "rxjs";
+import { cancellableTask } from "./cancellation-module";
 
 export const tabReadyPageMessage = createMessageDescription("PAGE/TAB_READY");
 
-export function waitForTabToBeReady(tabId: number): Promise<true> {
-  return listen(chrome.runtime.onMessage, (message: AppMessage, sender) => {
-    if (sender.tab?.id !== tabId || sender.id !== extensionId) {
-      return;
-    }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type EventPayload<E extends chrome.events.Event<any>> =
+  E extends chrome.events.Event<infer F>
+    ? // NOTE: cannot use `Parameters<F>` because `F` is inferred to `Function`
+      // which TS does not allow using in `Parameters<F>`
+      F extends (...args: infer A) => unknown
+      ? A
+      : never
+    : never;
 
-    if (!tabReadyPageMessage.is(message)) {
-      console.error("Unexpected message received", message);
-      return;
-    }
+const matchSenderExtensionId = ([_, sender]: EventPayload<
+  typeof chrome.runtime.onMessage
+>) => sender.id === extensionId;
 
-    return true;
-  });
-}
+const matchSenderTabId =
+  (tabId: number) =>
+  ([_, sender]: EventPayload<typeof chrome.runtime.onMessage>) =>
+    sender.tab?.id === tabId;
+
+export const waitForTabToBeReady = <Reason>(
+  tabId: number,
+  cancellationSignal: cancellableTask.CancellationSignal<Reason>
+): task.Task<cancellableTask.Cancellable<Reason, void>> => {
+  const tabReadyPageMessage$ = getEvent$(chrome.runtime.onMessage).pipe(
+    filter(matchSenderExtensionId),
+    filter(matchSenderTabId(tabId)),
+    map(([message]) => message as AppMessage),
+    filter(tabReadyPageMessage.is)
+  );
+
+  return pipe(
+    cancellableTask.fromObservable(cancellationSignal, tabReadyPageMessage$),
+    taskEither.map(() => undefined)
+  );
+};
