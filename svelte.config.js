268f1a068fb26d75f621436c0ad1987025900cc9
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-04-19 21:20:33 +0200

    chore: remove Snowpack
    Snowpack is very outdated now.
    
    This commit leaves the project without a build system. One (probably
    Vite) will be added soon.

--- svelte.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/no-var-requires */
-const autoPreprocess = require("svelte-preprocess");
-
-module.exports = {
-  preprocess: autoPreprocess(),
-};
--- web-test-runner.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable no-undef */
-// NODE_ENV=test - Needed by "@snowpack/web-test-runner-plugin"
-process.env.NODE_ENV = "test";
-
-module.exports = {
-  plugins: [require("@snowpack/web-test-runner-plugin")()],
-};
